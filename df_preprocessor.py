3abf1b47b2d5cc638d8d239717ae4543bd438388
Mosolygó <mbalazs@inf.u-szeged.hu>
2022-03-25 19:09:42 +0100

    First iteration of solution using xgboost

--- df_preprocessor.py
+++ df_preprocessor.py
@@ -6,9 +6,11 @@ from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, MinMaxScaler
 # used_column: a list of strings specifying which columns one wants to use
 # replaceUni: decide whether to fill the missing values of the enrolled_university column
 # shouldScale: decide whether to scale numeric values or not
-def load_and_process(used_columns: list, replaceUni:bool=True, shouldScale:bool=False) -> pd.DataFrame:
+def load_and_process(path: str, used_columns: list, replaceUni:bool=True, shouldScale:bool=False, shouldDropNA:bool=True) -> pd.DataFrame:
     # read data from csv
-    df = pd.read_csv("csv/train_input.csv")
+    df = pd.read_csv(path)
+    if('test_input.csv' in path):
+        df['target'] = np.load("csv/test_input_target.npy")
 
     # list if columns that, if called for should be encoded
     TO_ONEHOT = {'gender', 'major_discipline', 'company_type', 'city'}
@@ -25,24 +27,25 @@ def load_and_process(used_columns: list, replaceUni:bool=True, shouldScale:bool=
 
     # remove rows that are not called for, or contains missing values
     df = df[used_columns]
-    df = df.dropna(axis=0)
+    if shouldDropNA:
+        df = df.dropna(axis=0)
 
-    # encode columns according to the above defined lists
-    for to_encode in used_columns:
-        if to_encode in TO_ONEHOT:
-            res = one_hot_encoder.fit_transform(df[[to_encode]])
-            df = df.drop(to_encode, axis=1)
-            df = df.reset_index(drop=True)
-            # there are multiple onehotencoded values that start os at "Other", the original column name is concatenated so resolve the conflict
-            df = df.join(pd.DataFrame(res.toarray(), columns = [c_value + '_' + to_encode if c_value == 'Other' else c_value for c_value in one_hot_encoder.categories_[0]]))
-        elif to_encode in TO_ORDINAL:
-            df[to_encode] = ordinal_encoder.fit_transform(df[[to_encode]])
+        # encode columns according to the above defined lists
+        for to_encode in used_columns:
+            if to_encode in TO_ONEHOT:
+                res = one_hot_encoder.fit_transform(df[[to_encode]])
+                df = df.drop(to_encode, axis=1)
+                df = df.reset_index(drop=True)
+                # there are multiple onehotencoded values that start os at "Other", the original column name is concatenated so resolve the conflict
+                df = df.join(pd.DataFrame(res.toarray(), columns = [c_value + '_' + to_encode if c_value == 'Other' else c_value for c_value in one_hot_encoder.categories_[0]]))
+            elif to_encode in TO_ORDINAL:
+                df[to_encode] = ordinal_encoder.fit_transform(df[[to_encode]])
 
-    # the same min max scaler is used everywhere temporarily, may change during further development
-    if shouldScale:
-        for index, max_value in df.max().iteritems():
-            if max_value > 1 and index != 'enrollee_id':
-                df[index] = scaler.fit_transform(df[[index]])
+        # the same min max scaler is used everywhere temporarily, may change during further development
+        if shouldScale:
+            for index, max_value in df.max().iteritems():
+                if max_value > 1 and index != 'enrollee_id':
+                    df[index] = scaler.fit_transform(df[[index]])
 
     return df
 #%%
---
+++ xgboost_sol.py
@@ -0,0 +1,48 @@
+#%%
+import xgboost as xgb
+from xgboost import XGBClassifier
+import numpy as np
+from df_preprocessor import load_and_process
+from sklearn.metrics import f1_score, accuracy_score
+from scipy.stats import binom_test
+from sklearn.model_selection import GridSearchCV
+#%%
+keepList = ['city', 'city_development_index', 'relevent_experience', 'enrolled_university', 'education_level', 'major_discipline', 'experience', 'company_size', 'company_type', 'last_new_job', 'training_hours', 'target']
+df = load_and_process("csv/train_input.csv", keepList)
+#%%
+y_train = df['target']
+train = xgb.DMatrix(df.drop('target', axis=1).values, label=y_train.values)
+#%%
+test_df = load_and_process("csv/test_input.csv", keepList)
+y_test = test_df['target']
+test = xgb.DMatrix(test_df.drop('target', axis=1).values, label=y_test.values)
+# %%
+param = {
+    'max_depth': 4,
+    'eta' : 0.15,
+    'objective' : 'binary:hinge',
+    'min_child_weight' : 3
+}
+epochs = 10
+model = xgb.train(param, train, epochs)
+preds = model.predict(test)
+#f1_score(y_test, np.zeros(preds.shape[0]))
+
+test_acc = accuracy_score(y_test,preds)
+n_success = np.sum(y_test==preds)
+p=0.90
+interval = binom_test(n_success,y_test.shape[0],p=p)
+print("Test-acc: ",test_acc,"+/-",interval,"(",p*100,"%)")
+print("F1 score: " + str(f1_score(y_test, preds)))
+
+#%%
+
+params = [{
+    'max_depth': [4,5,6],
+    'eta' : [0.5, 0.1, 0.15, 0.2],
+    'min_child_weight' : [1,2,3]
+}]
+clf = GridSearchCV(XGBClassifier(objective='binary:hinge'), params, scoring='f1',cv=10, refit = True, return_train_score=True)
+clf.fit(df.drop('target', axis=1).values,y_train)
+print('Best-params:',clf.best_params_)
+print('Best-score:',clf.best_score_)
