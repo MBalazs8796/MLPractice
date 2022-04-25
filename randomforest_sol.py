52c8d54a1dcb3e8a9dc6dae3aa88dba28cf80eb8
Horváth János <h867265@stud.u-szeged.hu>
2022-04-25 16:55:27 +0200

    additional gridsearch params for randomforest

--- randomforest_sol.py
+++ randomforest_sol.py
@@ -22,11 +22,18 @@ print(accuracy_score(y_test,preds))
 print(f1_score(y_test, preds))
 
 params = {
-	'max_depth': [10, 11, 12, 13, 14, 15, 20, 25, 30, 40, 50, 75, 100, 1000]
+	'max_depth': [10, 11, 12, 13, 14, 15, 20, 25, 30, 40, 50, 75, 100, 1000],
+	'n_estimators': [10, 25, 50, 75, 90, 100, 110, 125, 150, 200, 300, 500, 1000],
+	'criterion': ['gini', 'entropy']
 }
 
-gs = GridSearchCV(RandomForestClassifier(), params, scoring='f1', cv=10, refit=True, return_train_score=True)
+gs = GridSearchCV(RandomForestClassifier(), params, scoring='f1', cv=10, refit=True, return_train_score=True, n_jobs=-1, verbose=10)
 gs.fit(x_train, y_train)
 
 print(gs.best_params_)
 print(gs.best_score_)
+
+"""
+{'criterion': 'entropy', 'max_depth': 14, 'n_estimators': 110}
+0.5334207409343039
+"""
